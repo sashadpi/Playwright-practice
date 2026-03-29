26d6b32637369f74cb55cefbfa09fdfc1006c71f
*Sasha* <*sashadpi68@gmail.com*>
2026-03-29 17:09:25 +0300

    feat: refactor API controllers and tests for improved readability and consistency

--- test/setup/auth.setup.ts
+++ test/setup/auth.setup.ts
@@ -1,18 +1,17 @@
-import { test as setup, expect} from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
 import { VALID_USER1, VALID_USER2 } from '../../test-data/users';
 import { authControllers } from '../../controllers/AuthControllers';
 
 setup.describe('Sign in and save storage state', () => {
+	setup('Sign in valid user 1 and save storage state', async ({ request }) => {
+		const authResponse = await authControllers.signIn(request, VALID_USER1.email, VALID_USER1.password);
+		expect(authResponse.status()).toBe(200);
+		await request.storageState({ path: './test-data/states/validUser1.json' });
+	});
 
-    setup('Sign in valid user 1 and save storage state', async ({ request }) => {
-        const authResponse = await authControllers.signIn(request, VALID_USER1.email, VALID_USER1.password);
-        expect(authResponse.status()).toBe(200);
-        await request.storageState({ path: './test-data/states/validUser1.json' });
-    });
-
-    setup('Sign in valid user 2 and save storage state', async ({ request }) => {
-        const authResponse = await authControllers.signIn(request, VALID_USER2.email, VALID_USER2.password);
-        expect(authResponse.status()).toBe(200);
-        await request.storageState({ path: './test-data/states/validUser2.json' });
-    });
+	setup('Sign in valid user 2 and save storage state', async ({ request }) => {
+		const authResponse = await authControllers.signIn(request, VALID_USER2.email, VALID_USER2.password);
+		expect(authResponse.status()).toBe(200);
+		await request.storageState({ path: './test-data/states/validUser2.json' });
+	});
 });
